f1110f7fc14ef7d0eaf15fa3853afb6aaf1421a7
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-03-04 19:55:54 -0300

    Initial commit

---
+++ src/main/java/entities/Conta.java
@@ -0,0 +1,89 @@
+package entities;
+
+public class Conta {
+    private int NumeroConta;
+    private String NomeTitularConta;
+    private double SaldoInicial;
+    private double Saldo;
+    private boolean IsInitialDeposit;
+
+    public boolean isInitialDeposit() {
+        return IsInitialDeposit;
+    }
+
+    public void setInitialDeposit(boolean initialDeposit) {
+        IsInitialDeposit = initialDeposit;
+    }
+
+    public Conta(){
+
+    }
+
+    public int getNumeroConta() {
+        return NumeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        NumeroConta = numeroConta;
+    }
+
+    public String getNomeTitularConta() {
+        return NomeTitularConta;
+    }
+
+    public void setNomeTitularConta(String nomeTitularConta) {
+        NomeTitularConta = nomeTitularConta;
+    }
+
+    public double getSaldoInicial() {
+        return SaldoInicial;
+    }
+
+    public void setSaldoInicial(double saldoInicial) {
+        SaldoInicial = saldoInicial;
+        if (SaldoInicial > 0)
+            setSaldo(saldoInicial);
+    }
+
+    public double getSaldo() {
+        return Saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        Saldo =+ saldo;
+    }
+
+    public void deposito(double valor) {
+        if (valor > 0)
+            setSaldo(valor - 5);
+    }
+
+    public void saque(double valor) {
+
+        if (IsInitialDeposit) {
+            setSaldo(valor);
+        }
+
+        double saldotemp = getSaldo();
+
+        if (saldotemp > valor) {
+            setSaldo((getSaldo() - valor) - 5);
+        } else if (saldotemp < valor) {
+            setSaldo((valor - getSaldo()) - 5);
+        } else if (saldotemp == valor) {
+            setSaldo((getSaldo() - valor) - 5);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Conta {" +
+                "NumeroConta=" + NumeroConta +
+                ", NomeTitularConta='" + NomeTitularConta + '\'' +
+                ", SaldoInicial=" + SaldoInicial +
+                ", Saldo=" + Saldo +
+                ", IsInitialDeposit=" + IsInitialDeposit + " " +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import entities.Conta;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Conta conta = new Conta();
+        System.out.println();
+
+        System.out.print("Entre com numero da conta:");
+        conta.setNumeroConta(Integer.parseInt(sc.nextLine()));
+
+        System.out.print("Entre com nome do cliente:");
+        conta.setNomeTitularConta(sc.nextLine());
+
+        System.out.print("Voce vai adcionar um deposito inicial (y/n):");
+        String opcoes = sc.next();
+
+        if (opcoes.equalsIgnoreCase("y")) {
+            conta.setInitialDeposit(true);
+        } else if (opcoes.equalsIgnoreCase("n")) {
+            conta.setInitialDeposit(false);
+        }
+
+        if (conta.isInitialDeposit()) {
+            System.out.println("Enter com valor do deposito inicial:");
+            conta.setSaldoInicial(sc.nextDouble());
+        }
+
+        System.out.println();
+
+        System.out.println("Dados da Conta:");
+        System.out.println(conta.toString());
+
+        System.out.println("Entre com um deposito:");
+        conta.deposito(sc.nextDouble());
+
+        System.out.println("Update nos Dados da conta:");
+        System.out.println(conta.toString());
+
+        System.out.println("Entre com um saque:");
+        conta.saque(sc.nextDouble());
+
+        System.out.println("Update nos Dados da conta:");
+        System.out.println(conta.toString());
+
+    }
+}
